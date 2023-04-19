fc90f0468d67a2536505d1640bfcb3f04fe08760
AlxAndr R1 <66573649+AlexanderARodin@users.noreply.github.com>
2023-04-19 03:51:58 +0300

    Create settings.lua

---
+++ nvim/lua/settings.lua
@@ -0,0 +1,12 @@
+opt.mouse               = 'a'
+
+opt.syntax              = 'on'
+opt.filetype            = 'on'
+opt.filetype.plugin     = 'on'
+
+opt.colorcolumn         = '60'
+opt.cursorline          = true
+opt.number              = true
+opt.relativenumber      = true
+opt.hlsearch            = true
+opt.incsearch           = true
