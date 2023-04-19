69d1eb34332d61893b0d96495ee4d666152e150a
AlxAndr R1 <66573649+AlexanderARodin@users.noreply.github.com>
2023-04-19 18:28:02 +0300

    Update settings.lua

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -8,7 +8,10 @@ opt.filetype.plugin     = 'on'
 
 opt.colorcolumn         = '60'
 opt.cursorline          = true
+opt.cursorcolumn        = true
+
 opt.number              = true
 opt.relativenumber      = true
+
 opt.hlsearch            = true
 opt.incsearch           = true
